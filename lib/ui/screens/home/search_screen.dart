473fcbf040122f3ee78944304621653fe10af1c2
Luan <69060192+luannn000@users.noreply.github.com>
2026-07-02 20:36:25 -0300

    Search Screen implementado

--- lib/ui/screens/home/search_screen.dart
+++ lib/ui/screens/home/search_screen.dart
@@ -10,6 +10,26 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsetsGeometry.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                hintText: "Buscar...",
+                border: OutlineInputBorder(),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Theme.of(context).colorScheme.outline,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
