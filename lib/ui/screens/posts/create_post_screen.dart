8e3c8459ec2be9f3b91cc8fd70755a97131d163f
xOmilk <leite46052@gmail.com>
2026-07-26 23:06:10 -0300

    feat: implement create post UI

--- lib/ui/screens/posts/create_post_screen.dart
+++ lib/ui/screens/posts/create_post_screen.dart
@@ -9,8 +9,26 @@ class CreatePostScreen extends StatefulWidget {
 }
 
 class _CreatePostScreenState extends State<CreatePostScreen> {
+  final TextEditingController _messageController = TextEditingController();
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+    super.dispose();
+  }
+
   void onGoingBack() {
     context.pop();
+  }
+
+  void onPublish() {
+    // Aqui você pode adicionar a lógica para salvar o post com o backend
+    final message = _messageController.text;
+    if (message.isNotEmpty) {
+      // ignore: avoid_print
+      print("Publicando: $message");
+      context.pop();
+    }
   }
 
   @override
@@ -18,9 +36,40 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
     return Scaffold(
       appBar: AppBar(
         leading: context.canPop()
-            ? IconButton(onPressed: onGoingBack, icon: Icon(Icons.arrow_back))
+            ? IconButton(onPressed: onGoingBack, icon: const Icon(Icons.close))
             : null,
-        title: Text("Criar post"),
+        title: const Text("Criar post"),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: FilledButton(
+              style: ButtonStyle(),
+              onPressed: onPublish,
+              child: const Text("Publicar"),
+            ),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: TextFormField(
+                  controller: _messageController,
+                  maxLines: null,
+                  autofocus: true,
+                  decoration: const InputDecoration(
+                    hintText: "O que você está pensando?",
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
